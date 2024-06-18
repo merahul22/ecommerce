7fd1ab4af5a55283aa73424e968ef9423b1303c0
merahul22 <rajrahul6572@gmail.com>
2024-06-18 12:42:57 +0700

    offers componets mounted in shop page

--- ecommerce/src/components/Items/Items.jsx
+++ ecommerce/src/components/Items/Items.jsx
@@ -7,10 +7,10 @@ const Items = (props) => {
 <p>{props.name}</p>
 <div className='item-prices'>
 <div className="item-prices-new">
-<p>{props.new_price}</p>
+<p>${props.new_price}</p>
 </div>
 <div className="item-prices-old">
-<p>{props.old_price}</p>
+<p>${props.old_price}</p>
 </div>
 </div>
     </div>
--- ecommerce/src/components/Popular/Popular.jsx
+++ ecommerce/src/components/Popular/Popular.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./Popular.css"
 import data_product from "../Assets/data.js"
-import Item from "../Items/Items.jsx"
+
+import Items from '../Items/Items.jsx'
 const Popular = () => {
   return (
     <div className='popular'>
@@ -10,7 +11,7 @@ const Popular = () => {
           <div className="popular-item">
             {data_product.map((item,i)=>{
                 return (
-               <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+               <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                 )
             })}
           </div>
