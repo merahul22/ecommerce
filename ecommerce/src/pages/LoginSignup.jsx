430040fd9ee0e779d85f5cf24838ad5d7e3c0d01
merahul22 <rajrahul6572@gmail.com>
2024-06-19 16:22:00 +0700

    logincss commit

--- ecommerce/src/pages/LoginSignup.jsx
+++ ecommerce/src/pages/LoginSignup.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css'
 const LoginSignup = () => {
   return (
     <div className='loginsignup'>
-<div className="loginignup-container">
+<div className="loginsignup-container">
 <h1>signup</h1>
 <div className="loginsignup-fields">
 <input type="text" placeholder='enter your name' />
 <input type="email" placeholder='enter your mail' />
 <input type="password" placeholder='enter your password'/>
 </div>
-<button>continue</button>
-<p>already have an account?<span>Login here </span></p>
-</div>
+<button>Continue</button>
+<p className='loginsignup-login'>already have an account?<span>Login here </span></p>
 <div className="loginsignup-agree">
   <input type="checkbox" name="" id="" />
   <p>By continuing ,i agree to term and policy</p>
+</div>
 </div>
     </div>
   )
