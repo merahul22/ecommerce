e27201e33847e823a51290a01f554ccee58c289a
merahul22 <rajrahul6572@gmail.com>
2024-06-19 16:22:24 +0700

    useparams to display product desciption

---
+++ ecommerce/src/components/ProductDisplay.jsx/ProductDisplay.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from "../Assets/star_icon.png"
+import stardull_icon from "../Assets/star_dull_icon.png"
+const ProductDisplay = (props) => {
+const {product}=props
+  return (
+    <div className='productdisplay'>
+<div className="productdisplay-left">
+<div className="productdisplay-img-list">
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+    <img src={product.image} alt="" />
+</div>
+<div className="productdisplay-img">
+    <img className='productdisplay-main-img' src={product.image} alt="" />
+</div>
+</div>
+<div className="productdisplay-right">
+    <h1>{product.name}</h1>
+    <div className="productdisplay-right-star">
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={star_icon} alt="" />
+        <img src={stardull_icon} alt="" />
+        <p>(122)</p>
+    </div>
+    <div className="productdisplay-right-prices">
+     <div className="productdisplay-right-prices-old">
+        ${product.old_price}
+     </div>
+     <div className="productdisplay-right-prices-new">
+        ${product.new_price}
+     </div>
+    </div>
+    <div className="productdisplay-right-description">
+       <p> A light weight and coloured cotten ,close fitting ,worn as underpant ,winterwear or outergarment </p>
+    </div>
+    <div className="productdisplay-right-size">
+        <h1>Select size</h1>
+   <div className="productdisplay-right-select-size">
+   <div>S</div>
+        <div>M</div>
+        <div>L</div>
+        <div>XL</div>
+        <div>XXL</div>
+   </div>
+    </div>
+    <button>add to cart</button>
+    <div className="productdisplay-right-category">
+        <p><span>Categpry:</span> Women,Tshirt, croptop</p>
+    </div>
+    <div className="productdisplay-right-category">
+        <p><span>Tags:</span> Modern , latest</p>
+    </div>
+
+
+</div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- ecommerce/src/pages/Product.jsx
+++ ecommerce/src/pages/Product.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
+import BreadCrum from '../components/BreadCrum/BreadCrum'
+import ProductDisplay from '../components/ProductDisplay.jsx/ProductDisplay'
 
 const Product = () => {
+  const {productId}=useParams()
+ const {all_product}=useContext(ShopContext)
+ const product=all_product.find((e)=>(Number(productId)===e.id))
+
   return (
-    <div>Product</div>
+    <div>
+<BreadCrum product={product}/>
+<ProductDisplay product={product}/>
+    </div>
   )
 }
 
